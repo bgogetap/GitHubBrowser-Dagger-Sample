b2c178cf608d35d64a269fb6f554e293063e56a1
Brandon Gogetap <bgogetap@gmail.com>
2020-04-25 14:35:37 -0700

    26-Home View model test

---
+++ home/src/test/java/com/sample/githubbrowser/home/HomeViewModelTest.kt
@@ -0,0 +1,66 @@
+package com.sample.githubbrowser.home
+
+import androidx.arch.core.executor.testing.InstantTaskExecutorRule
+import com.google.common.truth.Truth.assertThat
+import com.sample.githubbrowser.githubapi.GitHubApi
+import com.sample.githubbrowser.githubapi.model.RepoApiModel
+import com.sample.githubbrowser.githubapi.model.UserApiModel
+import com.sample.githubbrowser.home.list.RepoItem
+import com.sample.githubbrowser.repository.AppRepository
+import org.junit.Before
+import org.junit.Rule
+import org.junit.Test
+
+private val fakeRepoApiModel = RepoApiModel(
+    id = 1L,
+    name = "Mock Repo",
+    description = "Mock Repo Description",
+    owner = UserApiModel(id = 1L, login = "dagger"),
+    stargazersCount = 1,
+    forksCount = 1,
+    contributorsUrl = "",
+    createdDate = "1/1/2020",
+    updatedDate = "1/1/2020"
+)
+
+class HomeViewModelTest {
+
+    @get:Rule
+    val taskExecutorRule = InstantTaskExecutorRule()
+
+    private lateinit var viewModel: HomeViewModel
+    private lateinit var viewStateValues: MutableList<HomeViewState>
+
+    @Before
+    fun setUp() {
+        val appRepository = AppRepository(FakeGitHubApi())
+        viewStateValues = mutableListOf()
+
+        viewModel = HomeViewModel(appRepository)
+        viewModel.viewStateUpdates.observeForever { viewStateValues.add(it) }
+    }
+
+    @Test
+    fun `loaded state contains repo models`() {
+        assertThat(viewStateValues.size).isEqualTo(1)
+        val expectedState = HomeViewStateLoaded(
+            repos = listOf(
+                RepoItem(
+                    name = fakeRepoApiModel.name,
+                    description = fakeRepoApiModel.description,
+                    starsCount = fakeRepoApiModel.stargazersCount,
+                    forkCount = fakeRepoApiModel.forksCount
+                )
+            )
+        )
+
+        assertThat(viewStateValues[0]).isEqualTo(expectedState)
+    }
+}
+
+private class FakeGitHubApi : GitHubApi {
+
+    override fun getTopRepositories(): List<RepoApiModel> {
+        return listOf(fakeRepoApiModel)
+    }
+}
