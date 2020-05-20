0b5ce0f2a4dc514771df4912af504876d62d8b40
Brandon Gogetap <bgogetap@gmail.com>
2020-05-19 23:00:44 -0700

    41-AppRepository Updates

--- repository/src/main/java/com/sample/githubbrowser/repository/AppRepository.kt
+++ repository/src/main/java/com/sample/githubbrowser/repository/AppRepository.kt
@@ -1,6 +1,7 @@
 package com.sample.githubbrowser.repository
 
 import com.sample.githubbrowser.githubapi.GitHubApi
+import com.sample.githubbrowser.githubapi.model.ContributorApiModel
 import com.sample.githubbrowser.githubapi.model.RepoApiModel
 import javax.inject.Inject
 import javax.inject.Singleton
@@ -10,7 +11,26 @@ class AppRepository @Inject constructor(
     private val gitHubApi: GitHubApi
 ) {
 
+    private val cachedRepos = mutableListOf<RepoApiModel>()
+
     suspend fun getTopRepos(): List<RepoApiModel> {
-        return gitHubApi.getTopRepositories().items
+        if (cachedRepos.isEmpty()) {
+            cachedRepos.addAll(gitHubApi.getTopRepositories().items)
+        }
+        return cachedRepos
+    }
+
+    suspend fun getRepo(repoOwner: String, repoName: String): RepoApiModel {
+        return getRepoFromCache(repoOwner, repoName) ?: gitHubApi.getRepo(repoOwner, repoName)
+    }
+
+    suspend fun getContributors(repoOwner: String, repoName: String): List<ContributorApiModel> {
+        return gitHubApi.getContributors(repoOwner, repoName)
+    }
+
+    private fun getRepoFromCache(repoOwner: String, repoName: String): RepoApiModel? {
+        return cachedRepos.firstOrNull { repo ->
+            repo.owner.login == repoOwner && repo.name == repoName
+        }
     }
 }
